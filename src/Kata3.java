3b31400b2305ed0ae1b60f8cb9e97fa9452d0a9f
Usuario <Usuario@10.22.148.97>
2018-10-18 11:37:51 +0100

    Primera versión de Kata3

---
+++ src/Kata3.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Usuario
+ */
+public class Kata3 {
+    public static void main(String[] args){
+        HistogramDisplay histo = new HistogramDisplay();
+        histo.execute();
+    }
+}
